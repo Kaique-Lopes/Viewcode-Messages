d29119f15e155ba5fc6d82b4c235c4f90a8659fd
Kaique <kaique.lopes3271@gmail.com>
2022-02-06 20:32:53 -0300

    Create Extension

--- Viewcode-Messages/Viewcode-Messages/Utils/Extension+UIView.swift
+++ Viewcode-Messages/Viewcode-Messages/Utils/Extension+UIView.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIView{
+    func pin(to superview: UIView){
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.topAnchor.constraint(equalTo: superview.topAnchor).isActive = true
+        self.bottomAnchor.constraint(equalTo: superview.bottomAnchor).isActive = true
+        self.trailingAnchor.constraint(equalTo: superview.trailingAnchor).isActive = true
+        self.leadingAnchor.constraint(equalTo: superview.leadingAnchor).isActive = true
+    }
+}
