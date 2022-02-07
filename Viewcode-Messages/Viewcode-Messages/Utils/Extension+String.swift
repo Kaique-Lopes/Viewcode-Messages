181918be1b5aa006b5f92f924fd29df9411c92d7
Kaique <kaique.lopes3271@gmail.com>
2022-02-06 21:12:53 -0300

    Extension String

--- Viewcode-Messages/Viewcode-Messages/Utils/Extension+String.swift
+++ Viewcode-Messages/Viewcode-Messages/Utils/Extension+String.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import UIKit
+
+extension String {
+    func size(offFont font: UIFont) -> CGSize {
+        return (self as NSString).size(withAttributes: [NSAttributedString.Key.font : font])
+    }
+    
+    func heigtWithConstraintWidht(widht: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: widht, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: [.usesLineFragmentOrigin, .usesFontLeading], attributes: [NSAttributedString.Key.font : font], context: nil)
+        return boundingBox.height
+    }
+}
