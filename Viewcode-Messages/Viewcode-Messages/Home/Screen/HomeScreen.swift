5ae71fa5e488717b29952a1b437c070178db3aa3
Kaique <kaique.lopes3271@gmail.com>
2022-02-11 00:00:08 -0300

    HomeScreen

---
+++ Viewcode-Messages/Viewcode-Messages/Home/Screen/HomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreen.swift
+//  Viewcode-Messages
+//
+//  Created by Kaique Lopes de Oliveira on 11/02/22.
+//
+
+import UIKit
+
+class HomeScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
