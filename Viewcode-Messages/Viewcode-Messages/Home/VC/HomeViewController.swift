6ec37696daf5ee16259d86943ae9cf8b121ab780
Kaique <kaique.lopes3271@gmail.com>
2022-02-13 20:32:12 -0300

    screen and LoadView

--- Viewcode-Messages/Viewcode-Messages/Home/VC/HomeViewController.swift
+++ Viewcode-Messages/Viewcode-Messages/Home/VC/HomeViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    var screen : HomeScreen?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    override func loadView() {
+        self.screen = HomeScreen()
+        self.view = screen
+    }
 
 }
